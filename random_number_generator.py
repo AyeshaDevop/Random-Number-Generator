956c41e0cd314367680e7de608a1a2cdef4e2a88
AyeshaDevop <ashiiayesha123@gmail.com>
2024-12-05 13:08:01 +0500

    Done

---
+++ random_number_generator.py
@@ -0,0 +1,15 @@
+import random
+
+N_NUMBERS = 10
+MIN_VALUE = 1
+MAX_VALUE = 100
+
+def main():
+    """
+    Generates and prints 10 random numbers in the range 1 to 100.
+    """
+    for _ in range(N_NUMBERS):
+        print(random.randint(MIN_VALUE, MAX_VALUE))
+
+if __name__ == '__main__':
+    main()
